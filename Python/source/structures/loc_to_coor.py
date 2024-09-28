03336ccef51734e367a8bec87c95663009cb11e5
JULIAYOGA\Julia <jtoorn@michaelsoft.nl>
2024-09-28 13:53:57 +0200

    controle toegevoegd of er een locatie met de gegeven postcode bestaat én of de locatie in Nederland ligt

--- Python/source/structures/loc_to_coor.py
+++ Python/source/structures/loc_to_coor.py
@@ -2,31 +2,63 @@ from geopy.geocoders import Nominatim
 from .coordinates import Coordinates
 
 class LocationToCoordinates:
-    def __init__(self) -> None:
-        self._adres = {'street': 'leidsestraatweg', 'country': 'Nederland'} #{'street': 'Madame Curielaan', 'city': 'Rijswijk', 'country': 'Nederland'}
-        self._postcode = '2289CA'
+    def __init__(self, postcode) -> None:
+        self._adres = {'street': 'Madame Curielaan', 'city': 'Rijswijk', 'country': 'Nederland'} #{'street': 'leidsestraatweg', 'country': 'Nederland'} 
+        self._postcode = postcode
         self._locadress = None
         self._latitude: float = 0
         self._longitude: float = 0
     
-    def __str__(self) -> str:
-        return f"({self._postcode})"
+    def lat_lon_from_adress(self):
+        """
+        Genereer een Coordinates object aan de hand van een adres
+        """
 
-    def adress(self):
         # calling the Nominatim tool and create Nominatim class
         loc = Nominatim(user_agent="Geopy Library")
 
         # entering the location name
         getLoc = loc.geocode(self._adres)
 
+        #Dit kan uiteindelijk weggelaten worden
         self._locadress = getLoc.address
         self._latitude = getLoc.latitude
         self._longitude = getLoc.longitude
 
         #Coordinates object aanmaken
-        coordinate = Coordinates(self._latitude, self._longitude)
+        coordinate = Coordinates(getLoc.latitude, getLoc.longitude)
 
         return coordinate.coordinates
+    
+    def lat_lon_from_postcode(self):
+        """
+        Genereer een Coordinates object aan de hand van een postcode
+        """
+
+        # calling the Nominatim tool and create Nominatim class
+        loc = Nominatim(user_agent="Geopy Library")
+
+        # entering the location name
+        getLoc = loc.geocode(self._postcode)
+        
+        #controleren of er een locatie met de gegeven postcode is gevonden
+        if getLoc is None:
+            print("Postcode is niet correct of ligt niet in Nederland.")
+            return None
+        
+        #Dit kan uiteindelijk weggelaten worden
+        self._locadress = getLoc.address
+        self._latitude = getLoc.latitude
+        self._longitude = getLoc.longitude
+        
+        #controleren of de gevonden locatie in Nederland ligt
+        if self._locadress.endswith("Nederland"):
+            #Coordinates object aanmaken
+            coordinate = Coordinates(getLoc.latitude, getLoc.longitude)
+            return coordinate.coordinates
+        else:
+            print("Postcode ligt niet in Nederland.")
+            return None
     
     @property
     def latitude(self):
--- Python/tests/unit/test_loc_coor.py
+++ Python/tests/unit/test_loc_coor.py
@@ -2,8 +2,17 @@ import pytest
 from source.structures import LocationToCoordinates
 
 def test_location():
-    testlocatie = LocationToCoordinates()
-    print(testlocatie.adress())
+    postcode_nietbestaand = '2289 SD'
+    postcode_bestaand = '2289CA'
+    testlocatie = LocationToCoordinates(postcode_nietbestaand)
+    print("adres, postcode, werkelijk")
+    print(testlocatie.lat_lon_from_adress())
+    print(testlocatie.lat_lon_from_postcode())
+    print("(52.0484169, 4.3451929)")
+
+def test_Julia():
+    lat = 50.5000000
+    print(50 < lat < 54.5000)
 
 # Run the test
 if __name__ == "__main__":
