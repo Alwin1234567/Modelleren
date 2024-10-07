44bd123ce169c453aab54c7d7f1fa8a1e02c7ae2
JULIAYOGA\Julia <jtoorn@michaelsoft.nl>
2024-10-07 10:25:54 +0200

    mogelijke gegevens voor constantes toegevoegd

--- Python/source/constants/constants.py
+++ Python/source/constants/constants.py
@@ -5,9 +5,9 @@ class Constants:
     Een class die de constante waarden van de simulatie bevat.
     """
 
-    PRIJS_PER_KM = 0.0
-    PRIJS_PER_UUR_CHAUFFEUR = 0.0
-    TIJDSDUUR_INLADEN_EN_UITLADEN_PLAT = 0.0
+    PRIJS_PER_KM = 0.0  #bakwagen: 7.2 L/100km, bestelbus: 6.8 L/100km
+    PRIJS_PER_UUR_CHAUFFEUR = 0.0 #06:00-20:00 - 22.75, 20:00-00:00 - 29,575, 00:00-06:00 - 34,125
+    TIJDSDUUR_INLADEN_EN_UITLADEN_PLAT = 0.0 #5 tot 10 min
     TIJDSDUUR_INLADEN_EN_UITLADEN_INSTRUMENTENSETS = 0.0
     CAPACITEIT_VOERTUIG = 0
     MAPS_URL = "http://localhost:8989/route"
