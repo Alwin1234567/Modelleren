759a9d171d3b3d4d4a1c6146a1846a4cefc35844
Alwin1234567 <alwinvanderpaardt@gmail.com>
2024-10-24 11:18:38 +0200

    updated constants.py so it uses types rather than exact values

--- Python/tests/unit/test_constants.py
+++ Python/tests/unit/test_constants.py
@@ -2,18 +2,14 @@ import pytest
 from source.constants import Constants
 
 def test_constants():
-    assert Constants.PRIJS_PER_KM == 0.0
-    assert Constants.PRIJS_PER_UUR_CHAUFFEUR == 0.0
-    assert Constants.TIJDSDUUR_INLADEN_EN_UITLADEN_PLAT == 0.0
-    assert Constants.TIJDSDUUR_INLADEN_EN_UITLADEN_INSTRUMENTENSETS == 0.0
-    assert Constants.CAPACITEIT_VOERTUIG == 0
-    assert Constants.MAPS_URL == "http://localhost:8989/route"
-    assert Constants.MAPS_PARAMS == {
-        'profile': 'car',
-        'locale': 'en',
-        'calc_points': 'false'
-    }
-    assert Constants.STARTUP_WAIT_TIME == 5
+    assert isinstance(Constants.PRIJS_PER_KM, float)
+    assert isinstance(Constants.PRIJS_PER_UUR_CHAUFFEUR, float)
+    assert isinstance(Constants.TIJDSDUUR_INLADEN_EN_UITLADEN_PLAT, float)
+    assert isinstance(Constants.TIJDSDUUR_INLADEN_EN_UITLADEN_INSTRUMENTENSETS, float)
+    assert isinstance(Constants.CAPACITEIT_VOERTUIG, int)
+    assert isinstance(Constants.MAPS_URL, str)
+    assert isinstance(Constants.MAPS_PARAMS, dict)
+    assert isinstance(Constants.STARTUP_WAIT_TIME, int)
 
 # Run the test
 if __name__ == "__main__":
