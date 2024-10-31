0e5343c0796a682183e59ed2636ee06e44428f2a
JJuulT <125370674+JJuulT@users.noreply.github.com>
2024-10-31 11:34:20 +0100

    coördinaten functie (#13)
    * functies die van naam kliniek de locatie de coordinaten teruggeeft
    
    * unieke vraag toegevoegd (som barcode unieke waarden)
    
    * mogelijke gegevens voor constantes toegevoegd
    
    * hubs toegevoegd
    
    * alle klinieken die zelf vervoer regelen verwijderd
    
    * spaties achter # in commentaar
    
    ---------
    
    Co-authored-by: Alwin1234567 <alwinvanderpaardt@gmail.com>

--- Python/tests/unit/test_loc_coor.py
+++ Python/tests/unit/test_loc_coor.py
@@ -1,37 +1,18 @@
 import pytest
-from source.structures import LocationToCoordinates, ReadLocation, Coordinates
+from source.locations import Location, Location_type
 
-def test_location():
-    #bestaande postcode
-    postcode_bestaand = '2289 CA'
-    coordinates_bestaand = LocationToCoordinates(postcode_bestaand).lat_lon_from_postcode()
-    assert coordinates_bestaand.coordinates == Coordinates(52.04832912, 4.34559652).coordinates
-
-    #bestaande postcode zonder spatie
-    postcode_bestaand_2 = '2289CA'
-    coordinates_bestaand_2 = LocationToCoordinates(postcode_bestaand_2).lat_lon_from_postcode()
-    assert coordinates_bestaand_2.coordinates == Coordinates(52.04832912, 4.34559652).coordinates
-
-    #niet-bestaande postcode
-    postcode_nietbestaand = '2289 SD'
-    coordinates_nietbestaand = LocationToCoordinates(postcode_nietbestaand).lat_lon_from_postcode()
-    assert coordinates_nietbestaand == None
-
-    #niet-Nederlandse postcode
-    postcode_nietbestaand_2 = '2289'
-    coordinates_nietbestaand_2 = LocationToCoordinates(postcode_nietbestaand_2).lat_lon_from_postcode()
-    assert coordinates_nietbestaand_2 == None
-
-def test_csv_loc():
-    bestaand_ziekenhuis = 'Equipe Den Haag'
+def test_location_2():
+    bestaand_ziekenhuis = 'A Klinieken'
     nietbestaand_ziekenhuis = 'Niet-bestaand Ziekenhuis'
-    postcode_onbekend_ziekenhuis = 'A klinieken'
+    postcode_onbekend_ziekenhuis = 'Koning Kliniek'
+    coordinaten = Location(bestaand_ziekenhuis, Location_type.ZIEKENHUIS).name_to_coordinates()
     
-    assert ReadLocation(bestaand_ziekenhuis).postcode() == '2514 DH'
-    assert ReadLocation(nietbestaand_ziekenhuis).postcode() == None
-    assert ReadLocation(postcode_onbekend_ziekenhuis).postcode() == None
-
-
+    assert 52.10 <= coordinaten.lat <= 52.12
+    assert 5.24 <= coordinaten.lon <= 5.26
+    with pytest.raises(Exception, match="Deze locatie is niet bekend."):
+        Location(nietbestaand_ziekenhuis, Location_type.ZIEKENHUIS).name_to_coordinates()
+    with pytest.raises(Exception, match="Van deze locatie is geen postcode bekend."):
+        Location(postcode_onbekend_ziekenhuis, Location_type.ZIEKENHUIS).name_to_coordinates()
 
 # Run the test
 if __name__ == "__main__":
