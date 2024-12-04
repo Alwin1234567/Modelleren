5cb29a691e572aa12718866f73eb8a941b00b58e
Alwin1234567 <alwinvanderpaardt@gmail.com>
2024-12-04 13:13:01 +0100

    add baseline kosten functie

--- Python/source/main.py
+++ Python/source/main.py
@@ -1,4 +1,4 @@
-from flow import Create_locations, Metrieken, Verbeteringen, store_results
+from source.flow import Create_locations, Metrieken, Verbeteringen, store_results
 
 def run():
     creator = Create_locations()
